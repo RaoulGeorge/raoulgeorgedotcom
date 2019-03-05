04348d1c76de89a3ede5c11a268b9d11e93b1f98
Raoul George <raoulgeorge@gmail.com>
2019-03-05 18:50:12 +0530

    add react-router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import Home from './pages/Home';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/" exact component={Home} />
+        </div>
+      </Router>
     );
   }
 }
