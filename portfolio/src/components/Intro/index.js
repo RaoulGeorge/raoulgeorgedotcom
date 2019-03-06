b2eac5c6f9f844e204b02f840d630b1560de0a33
Raoul George <raoulgeorge@gmail.com>
2019-03-06 13:46:49 +0530

    add typist effect

--- portfolio/src/components/Intro/index.js
+++ portfolio/src/components/Intro/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import './intro.scss';
 
 const Intro = () => {
   return (
-    <div class="intro">
-      <h1> My name is Raoul George</h1>
-      <h2> I am a web developer</h2>
+    <div className="intro">
+      <h2>My name is Raoul George</h2>
+      <div className="iama">
+        <div className="static">
+          <h2>I am a</h2>
+        </div>
+        <ul className="effect-container">
+          <li className="effect-item">
+            <h2>web developer</h2>
+          </li>
+          <li className="effect-item">
+            <h2>proud father</h2>
+          </li>
+          <li className="effect-item">
+            <h2>warcraft addict</h2>
+          </li>
+          <li className="effect-item">
+            <h2>hardcore kopite</h2>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
